e103b1c0ec0c1f51336165c42d84f34ea2723316
JensonMedina <medinajenson74@gmail.com>
2023-11-04 19:48:59 -0300

    Proyecto creado como plantilla para tomar imagenes en winform usando la libreria aforge

---
+++ WebCamSolution/WebCamProjectPrincipal/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WebCamProjectPrincipal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.pbxGrabar = new System.Windows.Forms.PictureBox();
+            this.pbxCapturar = new System.Windows.Forms.PictureBox();
+            this.cbxGrabar = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGrabar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCapturar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(284, 38);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(82, 23);
+            this.btnGrabar.TabIndex = 0;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(616, 81);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(156, 23);
+            this.btnCapturar.TabIndex = 1;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // pbxGrabar
+            // 
+            this.pbxGrabar.Location = new System.Drawing.Point(12, 114);
+            this.pbxGrabar.Name = "pbxGrabar";
+            this.pbxGrabar.Size = new System.Drawing.Size(352, 273);
+            this.pbxGrabar.TabIndex = 2;
+            this.pbxGrabar.TabStop = false;
+            // 
+            // pbxCapturar
+            // 
+            this.pbxCapturar.Location = new System.Drawing.Point(420, 114);
+            this.pbxCapturar.Name = "pbxCapturar";
+            this.pbxCapturar.Size = new System.Drawing.Size(352, 273);
+            this.pbxCapturar.TabIndex = 3;
+            this.pbxCapturar.TabStop = false;
+            // 
+            // cbxGrabar
+            // 
+            this.cbxGrabar.FormattingEnabled = true;
+            this.cbxGrabar.Location = new System.Drawing.Point(29, 38);
+            this.cbxGrabar.Name = "cbxGrabar";
+            this.cbxGrabar.Size = new System.Drawing.Size(249, 21);
+            this.cbxGrabar.TabIndex = 4;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(575, 406);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(156, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cbxGrabar);
+            this.Controls.Add(this.pbxCapturar);
+            this.Controls.Add(this.pbxGrabar);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.btnGrabar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGrabar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCapturar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.PictureBox pbxGrabar;
+        private System.Windows.Forms.PictureBox pbxCapturar;
+        private System.Windows.Forms.ComboBox cbxGrabar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ WebCamSolution/WebCamProjectPrincipal/Form1.cs
@@ -0,0 +1,103 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCamProjectPrincipal
+{
+    public partial class Form1 : Form
+    {
+        //private OpenFileDialog Archivo = null;
+        private string path = @"C:\Users\chuni\OneDrive\Escritorio\Practicas\practicas C#\WebCamSolution\WebCamProjectPrincipal\ImagenesPerfil\";
+        private bool hayDispositivos;
+        private FilterInfoCollection misDispositivos; //Esto me sirve para obtener una lista de los dispositivos que hay en mi equipo instalados
+        private VideoCaptureDevice miWebCam; //Una vez que tengo un dispositivo elegido voy guardando los frames aca.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CargarDispositivos()
+        {
+            misDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if(misDispositivos.Count > 0)
+            {
+                hayDispositivos = true;
+                for(int i = 0; i < misDispositivos.Count; i++)
+                {
+                    cbxGrabar.Items.Add(misDispositivos[i].Name.ToString());
+                }
+                cbxGrabar.Text = misDispositivos[0].Name.ToString();
+            }
+            else
+            {
+                hayDispositivos = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDispositivos();
+        }
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap imagen = (Bitmap)eventArgs.Frame.Clone();
+            pbxGrabar.Image = imagen;
+            pbxGrabar.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+            int i = cbxGrabar.SelectedIndex;
+            string nombreVideo = misDispositivos[i].MonikerString;
+            miWebCam = new VideoCaptureDevice(nombreVideo);
+            miWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            miWebCam.Start();
+        }
+        private void CerrarWebCam()
+        {
+            if(miWebCam != null && miWebCam.IsRunning)
+            {
+                miWebCam.SignalToStop();
+                miWebCam = null;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CerrarWebCam();
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (miWebCam != null && miWebCam.IsRunning)
+            {
+                pbxCapturar.Image = pbxGrabar.Image;
+                pbxCapturar.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (!(File.Exists(path + "ClienteDni2.jpg")))
+            {
+                pbxCapturar.Image.Save(path + "ClienteDni2.jpg", ImageFormat.Jpeg);
+                MessageBox.Show("Imagen cargada con exito");
+            }
+            else
+            {
+                MessageBox.Show("La imagen que intenta guardar ya existe en la carpeta local");
+            }
+        }
+    }
+}
